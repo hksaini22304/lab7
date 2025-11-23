fd65ed3ee3ebb7b147cfa42fab55ab67fb54d249
Harjot <hsaini128@insite.4cd.edu>
2025-11-23 13:03:59 -0800

    Added Deezer proxy and endpoints

--- script.js
+++ script.js
@@ -8,4 +8,7 @@ btn. addEventListener("click", getPairing);
 // Bored API mirror
  const BORED_URL = "https://apis.scrimba.com/bored/api/activity";
 
+ // Deezer proxy + endpoints
+ const DEEZER_PROXY = "https://proxy.corsfix.com/?";
+ const DEEZER_ARTIST_SEARCH = "https://api.deezer.com/search/artist?q=";
  
