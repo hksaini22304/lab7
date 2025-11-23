33d4099f95ec6069bc613309aaaee15c0cf0b461
Harjot <hsaini128@insite.4cd.edu>
2025-11-23 13:01:35 -0800

    Connect UI button to getPairing function and set up DOM references for activity and playlist sections

---
+++ script.js
@@ -0,0 +1,7 @@
+const activityDiv = document.getElementById("activity");
+const playlistDiv = document.getElementById("playlist");
+const btn = document.getElementById("generateBtn");
+
+
+btn. addEventListener("click", getPairing);
+
