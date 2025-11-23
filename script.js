e100dc22c548914d44d0964ccb246158012640e6
Harjot <hsaini128@insite.4cd.edu>
2025-11-23 13:02:40 -0800

    Added BORED API

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const btn = document.getElementById("generateBtn");
 
 btn. addEventListener("click", getPairing);
 
+// Bored API mirror
+ const BORED_URL = "https://apis.scrimba.com/bored/api/activity";
+
+ 
