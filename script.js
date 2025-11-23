27a2c5331f5092d34f95571611d4ac26422d6602
Harjot <hsaini128@insite.4cd.edu>
2025-11-23 13:26:50 -0800

    Implement shuffleArray helper to randomize playlist track order

--- script.js
+++ script.js
@@ -173,4 +173,11 @@ function pickRandomArtists(list, n = 5) {
     return picked;
 }
 
-
+function shuffleArray(arr) {
+    const a = [...arr];
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
